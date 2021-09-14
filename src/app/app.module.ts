73b19a27b892e71b103e3247c658767a1e9afa19
v4724 <v47244724@gmail.com>
2021-09-14 17:18:48 +0800

    get-data-from-a-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from '@/app/app-routing.module';
-import { AppComponent } from '@/app/app.component';
-import { HeroesComponent } from '@/app/heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessageComponent } from './message/message.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from '@/app/app-routing.module'
+import { AppComponent } from '@/app/app.component'
+import { HeroesComponent } from '@/app/heroes/heroes.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'
+import { MessageComponent } from './message/message.component'
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
+import { InMemoryDataService } from '@/service/in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component'
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessageComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeroesComponent,
+		HeroDetailComponent,
+		MessageComponent,
+		DashboardComponent,
+  HeroSearchComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpClientModule,
+		HttpClientInMemoryWebApiModule.forRoot(
+			InMemoryDataService, { dataEncapsulation: false }
+		),
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,9 +19,27 @@ export class HeroesComponent implements OnInit {
 		this.getHeroes()
 	}
 
-	getHeroes(): void {
+	getHeroes (): void {
 		this.heroService.getHeroes()
-			.subscribe(heroes => this.heroes = heroes)
+		    .subscribe(heroes => this.heroes = heroes)
 	}
 
+	add (name: string): void {
+		name = name.trim()
+		if (!name) {
+			return
+		}
+
+		this.heroService.addHero({ name } as Hero)
+		    .subscribe(hero => {
+			    this.heroes.push(hero)
+		    })
+	}
+
+	delete (hero: Hero): void {
+		this.heroService.deleteHero(hero.id)
+		    .subscribe(_ =>
+			    this.heroes = this.heroes.filter(h => h !== hero)
+		    )
+	}
 }
